3f9a55f89453ece12acf258a19f6d87a14a71516
varadmondkar <varadmondkar@gmail.com>
2018-10-08 00:30:58 +0530

    Added BasePresenter

--- src/main/java/com/varad/base_module/BaseActivity.java
+++ src/main/java/com/varad/base_module/BaseActivity.java
@@ -10,8 +10,10 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.varad.base_module.Utils.CommonUtils;
+import com.varad.base_module.Utils.NetworkUtils;
 
 import butterknife.Unbinder;
 
@@ -62,6 +64,16 @@ public class BaseActivity extends AppCompatActivity implements BaseView {
     }
 
     /**
+     * Checks whether internet is connected or not
+     *
+     * @return
+     */
+    @Override
+    public boolean isConnectToInternet() {
+        return NetworkUtils.isInternetConnected(getApplicationContext());
+    }
+
+    /**
      * Showing Progress Dialog loader
      */
     @Override
@@ -78,6 +90,49 @@ public class BaseActivity extends AppCompatActivity implements BaseView {
         if (mProgressDialog != null && mProgressDialog.isShowing()) {
             mProgressDialog.cancel();
         }
+    }
+
+    @Override
+    public void onError(String errorMessage) {
+        if (errorMessage != null) {
+            showSnackBar(errorMessage);
+        } else {
+            showSnackBar("Some error occurred!!");
+        }
+
+    }
+
+    @Override
+    public void onError(int resId) {
+        onError(getString(resId));
+    }
+
+    @Override
+    public void showShortToastMessage(String message) {
+        if (message != null) {
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Some error occurred!!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void showShortToastMessage(int resId) {
+        showShortToastMessage(getString(resId));
+    }
+
+    @Override
+    public void showLongToastMessage(String message) {
+        if (message != null) {
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Some error occurred!!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void showLongToastMessage(int resId) {
+        showLongToastMessage(getString(resId));
     }
 
     public void showSnackBar(String message) {
--- src/main/java/com/varad/base_module/BaseView.java
+++ src/main/java/com/varad/base_module/BaseView.java
@@ -1,11 +1,27 @@
 package com.varad.base_module;
 
+import android.support.annotation.StringRes;
+
 /**
  * Created by varad on 29/09/18.
  */
 public interface BaseView {
 
+    boolean isConnectToInternet();
+
     void showLoading();
 
     void hideLoading();
+
+    void onError(String errorMessage);
+
+    void onError(@StringRes int resId);
+
+    void showShortToastMessage(String message);
+
+    void showShortToastMessage(@StringRes int resId);
+
+    void showLongToastMessage(String message);
+
+    void showLongToastMessage(@StringRes int resId);
 }
